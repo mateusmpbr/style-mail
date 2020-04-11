0cf98d8ad73f106a8fa638843ee44ee7994cc92b
Mateus Pereira <37028006+mRoxBR@users.noreply.github.com>
2020-04-11 20:18:08 -0300

    Refactory Editor

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 var myEditor;
 
 ClassicEditor
-    .create( document.querySelector( '#editor' ) )
+    .create( document.querySelector( '#editor' ),{
+        toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
+        heading: {
+            options: [
+                { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+            ]
+        }
+    } )
     .then( editor => {
         console.log( 'Editor was initialized', editor );
         myEditor = editor;
