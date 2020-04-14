10d52a55006b31fb1b63e1e72f32ede7bc5dcf95
Mateus Pereira <37028006+mRoxBR@users.noreply.github.com>
2020-04-14 18:14:22 -0300

    Update toolbar and placeholders

--- js/script.js
+++ js/script.js
@@ -2,12 +2,17 @@ var myEditor;
 
 ClassicEditor
     .create( document.querySelector( '#editor' ),{
-        toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
+        toolbar: [
+            'heading', '|',
+            'bold', 'italic', 'link', 'bulletedList', 'numberedList', '|',
+            'indent','outdent', '|',
+            'mediaEmbed', 'undo', 'redo'
+        ],
         heading: {
             options: [
-                { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-                { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-                { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+                { model: 'paragraph', title: 'Parágrafo', class: 'ck-heading_paragraph' },
+                { model: 'heading1', view: 'h1', title: 'Título 1', class: 'ck-heading_heading1' },
+                { model: 'heading2', view: 'h2', title: 'Título 2', class: 'ck-heading_heading2' }
             ]
         }
     } )
